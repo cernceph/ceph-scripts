014624b772cb888247646d2729a4835f293ee5ec
Dan van der Ster <daniel.vanderster@cern.ch>
2015-02-02 11:05:28 +0100

    crush-reweight-by-utilization: support reweight-by-pg usage

--- tools/crush-reweight-by-utilization.py
+++ tools/crush-reweight-by-utilization.py
@@ -2,8 +2,23 @@
 
 from cephinfo import cephinfo
 from optparse import OptionParser
+from collections import defaultdict
+import commands
 
-def reweight_by_utilization(oload):
+def get_weight(osd):
+  return osd_weights[osd]['reweight']
+
+def change_weight(osd, new_weight, really):
+  cmd = "ceph osd reweight %d %5f" % (osd, new_weight)
+  print cmd
+  if really:
+    (status, output) = commands.getstatusoutput(cmd)
+    if status:
+      raise Exception('Non-zero exit status (%d) from command: %s %s' % (status, cmd, output))
+  else:
+    print "add --really to run the above command"
+
+def reweight_by_utilization(oload, by_pg, pools, doit, really):
   if oload <= 100:
     raise Exception("You must give a percentage higher than 100.")
 #      "The reweighting threshold will be calculated as <average-utilization> "
@@ -13,32 +28,79 @@ def reweight_by_utilization(oload):
   cephinfo.init_pg()
   pgm = cephinfo.pg_data
 
-  # Avoid putting a small number (or 0) in the denominator when calculating
-  # average_util
-  if pgm['osd_stats_sum']['kb'] < 1024:
-    raise Exception("Refusing to reweight: we only have %d kB across all osds!" % pgm['osd_stats_sum']['kb'])
+  pgs_by_osd = defaultdict(int)
 
-  if pgm['osd_stats_sum']['kb_used'] < 5 * 1024:
-    raise Exception("Refusing to reweight: we only have %d kB across all osds!" % pgm['osd_stats_sum']['kb_used'])
+  if by_pg:
+    weight_sum = 0.0
+    num_pg_copies = 0
+    num_osds = 0
+    for p in pgm['pg_stats']:
+      pool = p['pgid'].split('.')[0]
+      if pools and pool not in pools:
+         continue
+      for a in p['acting']:
+        if not pgs_by_osd[a]:
+          num_osds += 1
+          weight_sum += get_weight(a)
+          pgs_by_osd[a] = 0
+        pgs_by_osd[a] += 1
+        num_pg_copies += 1
 
-  average_util = float(pgm['osd_stats_sum']['kb_used']) / float(pgm['osd_stats_sum']['kb'])
+    if not num_osds or (num_pg_copies / num_osds < 10):
+      raise Exception('Refusing to reweight: we only have %d PGs across %d osds!' % (num_pg_copies, num_osds))
+
+    average_util = num_pg_copies / weight_sum
+    print "weight_sum: %3f, num_pg_copies: %d, num_osds: %d" % (weight_sum, num_pg_copies, num_osds)
+
+  else:
+
+    # Avoid putting a small number (or 0) in the denominator when calculating
+    # average_util
+    if pgm['osd_stats_sum']['kb'] < 1024:
+      raise Exception("Refusing to reweight: we only have %d kB across all osds!" % pgm['osd_stats_sum']['kb'])
+
+    if pgm['osd_stats_sum']['kb_used'] < 5 * 1024:
+      raise Exception("Refusing to reweight: we only have %d kB across all osds!" % pgm['osd_stats_sum']['kb_used'])
+
+    average_util = float(pgm['osd_stats_sum']['kb_used']) / float(pgm['osd_stats_sum']['kb'])
+
+  # adjust down only if we are above the threshold
   overload_util = average_util * oload / 100.0
+
+  # but aggressively adjust weights up whenever possible
+  underload_util = average_util
 
   print "average_util: %04f, overload_util: %04f. " %(average_util, overload_util)
 
-  print "overloaded osds: "
-  changed = False
+  print "reweighted: "
 
   for osd in pgm['osd_stats']:
-    util = float(osd['kb_used']) / float(osd['kb'])
+    if by_pg:
+      util = pgs_by_osd[osd['osd']] / get_weight(osd['osd'])
+    else:
+      util = float(osd['kb_used']) / float(osd['kb'])
+
+    # skip the OSDs that do not contain anything (probably a different crush root)
+    if util < 0.01:
+      continue
+
     if util >= overload_util:
       # Assign a lower weight to overloaded OSDs. The current weight
       # is a factor to take into account the original weights,
       # to represent e.g. differing storage capacities
-      weight = osd_weights[osd['osd']]['crush_weight']
+      weight = get_weight(osd['osd'])
       new_weight = (average_util / util) * float(weight)
       print "%d [%04f -> %04f]" % (osd['osd'], weight, new_weight)
-
+      if doit: change_weight(osd['osd'], new_weight, really)
+    if util <= underload_util:
+      # assign a higher weight.. if we can
+      weight = get_weight(osd['osd'])
+      new_weight = (average_util / util) * float(weight)
+      if new_weight > 1.0:
+        new_weight = 1.0
+      if new_weight > weight:
+        print "%d [%04f -> %04f]" % (osd['osd'], weight, new_weight)
+        if doit: change_weight(osd['osd'], new_weight, really)
 
 def get_weights():
   cephinfo.init_crush()
@@ -58,10 +120,16 @@ def get_weights():
 if __name__ == "__main__":
   import sys
   parser = OptionParser()
-  parser.add_option("-o", "--oload", dest="oload", type="float",
-                  help="The reweighting threshold will be calculated as <average-utilization>")
+  parser.add_option("-o", "--overload", dest="oload", type="float", default=120.0,
+                  help="The overload threshold percentage, default 120%")
+  parser.add_option("-b", "--by_pg", dest="by_pg", action="store_true",
+                  help="Reweight by num PGs instead of utilization")
+  parser.add_option("-p", "--pool", dest="pools", action="append",
+                  help="Only work on these pools.")
+  parser.add_option("-d", "--doit", dest="doit", action="store_true",
+                  help="Do it!")
+  parser.add_option("-r", "--really", dest="really", action="store_true",
+                  help="Really really do it! This will change your crush map.")
   (options, args) = parser.parse_args()
-  if not options.oload:   # if filename is not given
-    parser.error('OLOAD not given')
   get_weights()
-  reweight_by_utilization(options.oload)
+  reweight_by_utilization(options.oload, options.by_pg, options.pools, options.doit, options.really)
