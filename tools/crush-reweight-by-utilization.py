c1805b78aa4457dca0e1d080666f18c9811b9546
Dan van der Ster <daniel.vanderster@cern.ch>
2016-07-27 14:47:41 +0200

    Add --bucket option, calc util by crush weight, remove short options

--- tools/crush-reweight-by-utilization.py
+++ tools/crush-reweight-by-utilization.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from cephinfo import cephinfo
+import ceph_osds_in_bucket
 from optparse import OptionParser
 from collections import defaultdict
 import commands
@@ -28,9 +29,6 @@ def change_weight(osd, new_weight, really):
 def reweight_by_utilization(options):
   if options.oload <= 100:
     raise Exception("You must give a percentage higher than 100.")
-#      "The reweighting threshold will be calculated as <average-utilization> "
-#      "times <input-percentage>. For example, an argument of 200 would "
-#      "reweight OSDs which are twice as utilized as the average OSD.\n";
 
   cephinfo.init_pg()
   pgm = cephinfo.pg_data
@@ -61,8 +59,7 @@ def reweight_by_utilization(options):
 
   else:
     num_osd = len(pgm['osd_stats'])
-    # Avoid putting a small number (or 0) in the denominator when calculating
-    # average_util
+    # Avoid putting a small number (or 0) in the denominator when calculating average_util
     if pgm['osd_stats_sum']['kb'] * 1024 / num_osd < mon_reweight_min_bytes_per_osd:
       raise Exception("Refusing to reweight: we only have %d kB across all osds!" % pgm['osd_stats_sum']['kb'])
 
@@ -71,31 +68,57 @@ def reweight_by_utilization(options):
 
     average_util = float(pgm['osd_stats_sum']['kb_used']) / float(pgm['osd_stats_sum']['kb'])
 
+  print "Found %d OSDs in total" % len(pgm['osd_stats'])
+
+  # filter out the empty osds
+  nonempty_osds = [ osd for osd in pgm['osd_stats'] if float(osd['kb']) > 0 and get_weight(osd['osd'],type='crush_weight') > 0 ]
+
+  print "Found %d non-empty OSDs" % len(nonempty_osds)
+
+  # optionally filter out osds not in the requested bucket
+  # and recalculate average_util
+  if options.bucket:
+    bucket_osds = []
+    for bucket in options.bucket:
+      bucket_osds.extend(ceph_osds_in_bucket.list(bucket))
+    sum_kb = 0
+    sum_weight = 0
+    sum_kb_used = 0
+    filtered_osds = []
+    for osd in nonempty_osds:
+      name = 'osd.%d' % osd['osd']
+      if name in bucket_osds:
+        sum_weight += get_weight(osd['osd'], 'crush_weight') * 1024*1024*1024
+        sum_kb_used += osd['kb_used']
+        filtered_osds.insert(0, osd)
+    average_util = float(sum_kb_used) / float(sum_weight)
+    print "Found %d OSDs after filtering by bucket" % len(filtered_osds)
+  else:
+    filtered_osds = nonempty_osds
+
+
+  # sort osds from most to least deviant from the average_util
+  if options.by_pg:
+    osds = sorted(filtered_osds, key=lambda osd: -abs(average_util - pgs_by_osd[osd['osd']] / get_weight(osd['osd'],type='crush_weight')))
+  else:
+    #osds = sorted(filtered_osds, key=lambda osd: -abs(average_util - float(osd['kb_used']) / float(osd['kb'])))
+    osds = sorted(filtered_osds, key=lambda osd: -abs(average_util - float(osd['kb_used']) / (get_weight(osd['osd'],type='crush_weight') * 1024*1024*1024)))
+
   # adjust down only if we are above the threshold
   overload_util = average_util * options.oload / 100.0
 
   # but aggressively adjust weights up whenever possible
-  underload_util = average_util # - (overload_util - average_util)
+  underload_util = average_util
 
   print "average_util: %04f, overload_util: %04f, underload_util: %04f. " %(average_util, overload_util, underload_util)
 
-  print "reweighted: "
-
-  # sort to get heaviest osds first
-
-  nonempty_osds = [ osd for osd in pgm['osd_stats'] if float(osd['kb']) > 0 and get_weight(osd['osd'],type='crush_weight') > 0 ]
-
-  if options.by_pg:
-    osds = sorted(nonempty_osds, key=lambda osd: -abs(average_util - pgs_by_osd[osd['osd']] / get_weight(osd['osd'],type='crush_weight')))
-  else:
-    osds = sorted(nonempty_osds, key=lambda osd: -abs(average_util - float(osd['kb_used']) / float(osd['kb'])))
-  
+  print "reweighting: "
   n = 0
   for osd in osds:
     if options.by_pg:
       util = pgs_by_osd[osd['osd']] / get_weight(osd['osd'],type='crush_weight')
     else:
-      util = float(osd['kb_used']) / float(osd['kb'])
+      util = float(osd['kb_used']) / (get_weight(osd['osd'],type='crush_weight') * 1024*1024*1024)
 
     # skip the OSDs that do not contain anything (probably a different crush root)
     if util < 0.01:
@@ -112,7 +135,7 @@ def reweight_by_utilization(options):
       if options.doit: change_weight(osd['osd'], new_weight, options.really)
       n += 1
       if n >= options.num_osds: break
-    if options.adjust_up and util <= underload_util:
+    if not options.no_increasing and util <= underload_util:
       # assign a higher weight.. if we can
       weight = get_weight(osd['osd'])
       new_weight = (average_util / util) * float(weight)
@@ -143,22 +166,28 @@ def get_weights():
 if __name__ == "__main__":
   import sys
   parser = OptionParser()
-  parser.add_option("-o", "--overload", dest="oload", type="float", default=120.0,
+  parser.add_option("--overload", dest="oload", type="float", default=120.0,
                   help="The overload threshold percentage, default 120%")
-  parser.add_option("-b", "--by-pg", dest="by_pg", action="store_true",
+  parser.add_option("--by-pg", dest="by_pg", action="store_true",
                   help="Reweight by num PGs instead of utilization")
-  parser.add_option("-p", "--pool", dest="pools", action="append",
+  parser.add_option("--pool", dest="pools", action="append",
                   help="Only work on these pools.")
-  parser.add_option("-u", "--adjust-up", dest="adjust_up", action="store_true",
+  parser.add_option("--no-increasing", dest="no_increasing", action="store_true",
                   help="Also adjust weights up if OSDs are below ideal weight")
-  parser.add_option("-m", "--max-change", dest="max_change", type="float", default=0.05,
-                  help="Maximum weight change to each OSD (default 0.1)")
-  parser.add_option("-n", "--num-osds", dest="num_osds", type="int", default=4,
+  parser.add_option("--max-change", dest="max_change", type="float", default=0.05,
+                  help="Maximum weight change to each OSD (default 0.05)")
+  parser.add_option("--num-osds", dest="num_osds", type="int", default=4,
                   help="Number of OSDs to change (default 4)")
-  parser.add_option("-d", "--doit", dest="doit", action="store_true",
+  parser.add_option("--doit", dest="doit", action="store_true",
                   help="Do it!")
-  parser.add_option("-r", "--really", dest="really", action="store_true",
+  parser.add_option("--really", dest="really", action="store_true",
                   help="Really really do it! This will change your crush map.")
+  parser.add_option("--bucket", action="append",
+                    help="Only reweight OSDs in this CRUSH bucket")
   (options, args) = parser.parse_args()
+
+  if options.bucket and options.by_pg:
+    raise Exception("Use of --by-pg and --bucket at the same time is not implemented")
+
   get_weights()
   reweight_by_utilization(options)
