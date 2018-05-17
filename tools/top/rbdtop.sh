399fc4b8244dc93ff8a6783a5681f787b225caf8
Julien Collet <julien.collet@cern.ch>
2018-05-17 15:40:12 +0200

    rbdtop: implement backbone of rbdtop script
    Signed-off-by: Julien Collet <julien.collet@cern.ch>

---
+++ tools/top/rbdtop.sh
@@ -0,0 +1,24 @@
+# !/bin/bash 
+#
+#
+# ./rbdtop.sh <osd> <time frame>
+# <osd> the id of the osd under scrutiny
+# <time_frame> log gathering period
+
+RBDTOP='\033[1;31m\033[40m[rbdtop]\033[0m'
+
+
+# activate appropriate debug level 
+echo -e "${RBDTOP} Adjusting debug level to osd.$1"
+ceph tell osd.$1 injectargs --debug_ms 1
+
+# wait for a while
+echo -e "${RBDTOP} Gathering log for $2 secs"
+sleep $2;
+
+# gather some logs
+echo -e "${RBDTOP} Logs collected, parsing."
+echo -e "${RBDTOP} logfile is: \n" `ls /var/log/ceph/ceph-osd.$1.log`
+
+# deactivate logging before exit
+ceph tell osd.$1 injectargs --debug_ms 0
