552323dcaa9132f100d8fff1f4e860be2089023f
Julien Collet <julien.collet@cern.ch>
2018-05-25 17:21:09 +0200

    rbdtop: fixing print output in per-image script
    Signed-off-by: Julien Collet <julien.collet@cern.ch>

--- tools/top/rbd_image_stats.sh
+++ tools/top/rbd_image_stats.sh
@@ -4,12 +4,12 @@
 # usage: ./rbdtop <osd num> <image id>
 #
 
-echo -e "\033[1;41m\033[40m[`date '+%F %T'`:rbdtop:$2]\033[0m Showing usage of image $2 (osd.$1)"
+echo -e "\033[1;41m\033[40m[`date '+%F %T'`/rbdtop/$2]\033[0m Showing usage of image $2 (osd.$1)"
 
 # TODO: write
 # Total write size
 # Average write size +(min/max) +count
-echo -e "\033[1;41m\033[40m[`date '+%F %T'`:rbdtop:$2]\033[0m Write ops summary"
+echo -e "\033[1;41m\033[40m[`date '+%F %T'`/rbdtop/$2]\033[0m Write ops summary"
 cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[writefull [0-9]+~[0-9]+\]" | uniq -c | tr -d "][" | sed 's/~/ /'
 cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[write [0-9]+~[0-9]+\]" | uniq -c | tr -d "][" | sed 's/~/ /'
 
@@ -17,7 +17,7 @@ cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[write [0-9]+~[0-
 # TODO: read
 # Total read size
 # Average read size +(min/max) +count
-echo -e "\033[1;41m\033[40m[`date '+%F %T'`:rbdtop:$2]\033[0m Read ops summary"
+echo -e "\033[1;41m\033[40m[`date '+%F %T'`/rbdtop/$2]\033[0m Read ops summary"
 cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[sparse-read [0-9]+~[0-9]+\]" | uniq -c | tr -d "][" | sed 's/~/ /'
 cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[read [0-9]+~[0-9]+\]" | uniq -c | tr -d "][" | sed 's/~/ /'
 
