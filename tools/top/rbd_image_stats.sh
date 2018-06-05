e14c88072d69dfaaf41934958b55fbef815af494
Julien Collet <julien.collet@cern.ch>
2018-06-05 10:50:33 +0200

    rbdtop/image_stats: sort transactions before grouping them
    Signed-off-by: Julien Collet <julien.collet@cern.ch>

--- tools/top/rbd_image_stats.sh
+++ tools/top/rbd_image_stats.sh
@@ -10,15 +10,15 @@ echo -e "\033[1;41m\033[40m[`date '+%F %T'`/rbdtop/$2]\033[0m Showing usage of i
 # Total write size
 # Average write size +(min/max) +count
 echo -e "\033[1;41m\033[40m[`date '+%F %T'`/rbdtop/$2]\033[0m Write ops summary"
-cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[writefull [0-9]+~[0-9]+\]" | uniq -c | tr -d "][" | sed 's/~/ /'
-cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[write [0-9]+~[0-9]+\]" | uniq -c | tr -d "][" | sed 's/~/ /'
+cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[writefull [0-9]+~[0-9]+\]" | sort | uniq -c | tr -d "][" | sed 's/~/ /'
+cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[write [0-9]+~[0-9]+\]" | sort | uniq -c | tr -d "][" | sed 's/~/ /'
 
 
 # TODO: read
 # Total read size
 # Average read size +(min/max) +count
 echo -e "\033[1;41m\033[40m[`date '+%F %T'`/rbdtop/$2]\033[0m Read ops summary"
-cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[sparse-read [0-9]+~[0-9]+\]" | uniq -c | tr -d "][" | sed 's/~/ /'
-cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[read [0-9]+~[0-9]+\]" | uniq -c | tr -d "][" | sed 's/~/ /'
+cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[sparse-read [0-9]+~[0-9]+\]" | sort | uniq -c | tr -d "][" | sed 's/~/ /'
+cat /var/log/ceph/ceph-osd.$1.log | grep -E "$2"  | grep -oE "\[read [0-9]+~[0-9]+\]" | sort | uniq -c | tr -d "][" | sed 's/~/ /'
 
 
