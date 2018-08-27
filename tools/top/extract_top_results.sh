4167264b67193905dd234367d463be0c3c0fd983
Julien Collet <julien.collet@cern.ch>
2018-08-27 15:53:41 +0200

    Minor fixes & improvements
    Signed-off-by: Julien Collet <julien.collet@cern.ch>

--- tools/top/extract_top_results.sh
+++ tools/top/extract_top_results.sh
@@ -1,16 +1,36 @@
 #!/bin/bash
 #
-# Usage: ./extract_top_results.sh <path to files> <displaysize>
+# Usage: ./extract_top_results.sh <path to files> [<output folder>]
 #
 
+if [ -z $2 ]
+then 
+  echo "No output folder provided. Default to /tmp/"
+else
+  echo "Output folder is $2"
+  of=$2"/"
+  if [ ! -d $2 ]
+  then 
+    mkdir $of
+  fi
+fi
 
+for token in "write" "read" "sparse-read" "writefull";
+do
+  echo $token
+  for img in `cat $1/* | ./extract_images.awk $token | sort | uniq -c | sort -k1gr | grep -Eo "rbd_data\.[0-9a-z]+"`;
+  do
+    echo $img
+    for imginfile in `grep -E "$img" -R $1 -l`; 
+    do
+      echo -n $imginfile" " | grep -Eo "[0-9]{4}\-[0-9]{2}-[0-9]{2}-[0-2][0-9]:[0-5][0-9]:[0-5][0-9]" | tr -d "\n" >> $of/$img.$token.out;
+      cat $imginfile | ./extract_images.awk $token 1 | grep $img  >> $of/$img.$token.out
+    done
+    # output to img.token.out
+  done
+  echo ""
+done
 
-grep -E "\- writefull:" -R $1 -A 15 | grep -Eo "rbd_data.[a-z0-9]+" | sort | uniq -c | sort -h | tail -n "$2"
-echo ""
-
-grep -E "\- write:" -R $1 -A 15 | grep -Eo "rbd_data.[a-z0-9]+" | sort | uniq -c | sort -h | tail -n "$2"
-echo ""
-
-grep -E "\- read:" -R $1 -A 15 | grep -Eo "rbd_data.[a-z0-9]+" | sort | uniq -c | sort -h | tail -n "$2"
-echo ""
-
+#
+# TODO: Consider removing rbd image id in output file as featured in the filename itself
+#
