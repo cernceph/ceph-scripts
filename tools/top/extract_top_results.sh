d2169f3025505db8df1b9adea3fb330315dd8a36
Julien Collet <julien.collet@cern.ch>
2018-08-29 16:55:19 +0200

    minor fixes
    Signed-off-by: Julien Collet <julien.collet@cern.ch>

--- tools/top/extract_top_results.sh
+++ tools/top/extract_top_results.sh
@@ -5,7 +5,9 @@
 
 if [ -z $2 ]
 then 
-  echo "No output folder provided. Default to /tmp/"
+  echo "No output folder provided. Default to /tmp/output/"
+  mkdir /tmp/output
+  of=/tmp/output/
 else
   echo "Output folder is $2"
   of=$2"/"
@@ -15,13 +17,15 @@ else
   fi
 fi
 
+rm -f $of/rbd_data*
+
 for token in "write" "read" "sparse-read" "writefull";
 do
   echo $token
   for img in `cat $1/* | ./extract_images.awk $token | sort | uniq -c | sort -k1gr | grep -Eo "rbd_data\.[0-9a-z]+"`;
   do
     echo $img
-    for imginfile in `grep -E "$img" -R $1 -l`; 
+    for imginfile in `grep -E "$img" -R $1 -l | sort`; 
     do
       timestamp=`echo -n $imginfile" " | grep -Eo "[0-9]{4}\-[0-9]{2}-[0-9]{2}-[0-2][0-9]:[0-5][0-9]:[0-5][0-9]" | tr -d "\n"`
       imagestats=`cat $imginfile | ./extract_images.awk $token 1 | grep $img | tr -d "\n"`
@@ -34,6 +38,21 @@ do
   done
   echo ""
 done
+
+#
+# GNUPLOT Script generation
+#
+
+cat prefix_gnuplot.template > $of/plot.gnu
+
+echo plot \\ >> $of/plot.gnu ; 
+cnt=0;
+for i in `wc -l $of/* | grep -v total | sort -k1g | tr -d " " | sed 's/^[0-9]*//g'`; 
+do
+  echo \"$i\" using 1:2 w lp lt $cnt, \\ >> $of/plot.gnu;
+  cnt=$((cnt+1));
+done
+
 
 #
 # TODO: Consider removing rbd image id in output file as featured in the filename itself
