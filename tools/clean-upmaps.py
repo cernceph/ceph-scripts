3e9779079d32d7de0dd99268856c9672585196ae
Dan van der Ster <daniel.vanderster@cern.ch>
2018-10-11 10:11:23 +0200

    clean-upmaps: check a few more error cases

--- tools/clean-upmaps.py
+++ tools/clean-upmaps.py
@@ -9,9 +9,21 @@ osd_dump_json = commands.getoutput('ceph osd dump -f json')
 osd_dump = json.loads(osd_dump_json)
 upmaps = osd_dump['pg_upmap_items']
 
+pgs_json = commands.getoutput('ceph pg ls -f json')
+pgs = json.loads(pgs_json)
+
+up = {}
+for pg in pgs:
+  up[pg['pgid']] = pg['up']
+
 for pg in upmaps:
   pgid = str(pg['pgid'])
   f = [x['from'] for x in pg['mappings']]
   t = [x['to'] for x in pg['mappings']]
   if len(f) != len(set(f)) or len(t) != len(set(t)):
-    print pgid,'has osds duplicated in the from or to set',pg['mappings']
+    print pgid, 'upmap has osds duplicated in the from or to set', pg['mappings']
+  if len(f+t) != len(set(f+t)):
+    print pgid, 'upmap has osds duplicated:', pg['mappings']
+  for x in pg['mappings']:
+    if x['to'] not in up[pgid]:
+      print pgid, 'upmap has "to" osds which are not in the up set', pg['mappings']
