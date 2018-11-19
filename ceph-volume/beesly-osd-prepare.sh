c5c8773ea0e890ef783c62fbeb9b50093c50dfa1
Dan van der Ster <daniel.vanderster@cern.ch>
2018-11-19 13:56:33 +0100

    beesly-osd-prepare: remove destroy option

--- ceph-volume/beesly-osd-prepare.sh
+++ ceph-volume/beesly-osd-prepare.sh
@@ -1,4 +1,4 @@
-ceph-volume lvm zap /dev/sd[a-x] /dev/sda[a-d] --destroy
+ceph-volume lvm zap /dev/sd[a-x] /dev/sda[a-d]
 ceph-volume lvm batch /dev/sd[a-f] /dev/sdaa --yes
 ceph-volume lvm batch /dev/sd[g-l] /dev/sdab --yes
 ceph-volume lvm batch /dev/sd[m-r] /dev/sdac --yes
