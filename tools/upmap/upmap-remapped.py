ff22ebb0470cf4465ea7bbaab40c0e01b1f64b0a
Dan van der Ster <daniel.vanderster@cern.ch>
2019-01-30 10:50:12 +0100

    upmap-remapped: really only print first 50 upmaps

--- tools/upmap/upmap-remapped.py
+++ tools/upmap/upmap-remapped.py
@@ -59,7 +59,7 @@ def gen_upmap_erasure(up, acting):
 
 def upmap_pg_items(pgid, mapping):
   print('ceph osd pg-upmap-items %s ' % pgid, end='')
-  for pair in mapping[0:50]:
+  for pair in mapping:
     print('%s %s ' % pair, end='')
   print('&')
 
@@ -112,7 +112,7 @@ for pg in upmaps:
    has_upmap[pgid] = True
 
 # handle each remapped pg
-for pg in remapped:
+for pg in remapped[0:50]:
   pgid = pg['pgid']
 
   # skip the degraded pgs
