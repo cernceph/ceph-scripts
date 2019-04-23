8e6e4e078204b22e833ec6b4c166ae620da66b52
Julien Collet <julien.collet@cern.ch>
2019-04-23 11:02:45 +0200

    recreate-osd: improvements
    Signed-off-by: Julien Collet <julien.collet@cern.ch>

--- tools/ceph-disk-replacement/recreate-osd.sh
+++ tools/ceph-disk-replacement/recreate-osd.sh
@@ -51,34 +51,34 @@ function draw(){
     fi
 }
 
+if [[ -z $DEV ]];
+then
+  echo "echo \"No drive passed\""
+  exit
+fi
+
+if [[ `echo $DEV | grep -Eo "/dev/sd[a-z][a-z]?" -c` -eq 0 ]];
+then
+  echo "echo \"Argument malformed, check spelling\""
+  exit
+fi
+
+echo $INITSTATE | grep -q "HEALTH_OK"
+if [[ $? -eq 1 ]]; 
+then
+  if [[ $FORCEMODE -eq 0 ]];
+  then
+    echo "echo \"Ceph is $INITSTATE, aborting\""
+    echo "echo \"Use -f to force execution\""
+    exit
+  else
+    draw "# Ceph is $INITSTATE"
+  fi
+fi
+
 
 if [[ -z $OSD ]];
 then
-    draw "no OSD provided, will autodetermine later on"
-fi
-
-draw "Checking ceph health"
-draw $INITSTATE
-
-
-if [[ `echo $INITSTATE | grep -q "HEALTH_OK"` -eq 1 ]]; 
-then
-    if [[ $FORCEMODE -eq 0 ]];
-    then
-        draw "Ceph is unhealthy, aborting"
-        exit
-    else
-        draw "Ceph is unhealthy"
-    fi
-else
-    draw "Ceph is healthy"
-fi
-
-
-
-if [[ -z $OSD ]];
-then
-    #autodetermine OSD
     AWKHOST=`echo $HOSTNAME | sed 's/.cern.ch//'`
     OSD=`ceph osd tree down | awk -v awkhost=$AWKHOST 'BEGIN { out=0 } { if($0 ~ /rack/) {out=0} if(out) {print $0; out=0} if($0 ~ awkhost) {out=1}; }' | grep -Eo "osd\.[0-9]+" | tr -d "[a-z\.]"`
 fi
@@ -101,7 +101,6 @@ then
     DBD=`ceph-volume lvm list | awk -v awkosdid=osd.$OSD 'BEGIN { out=0 } { if($0 ~ /====/) {out=0} if(out) {print $0;} if($0 ~ awkosdid) {out=1}; }'  | grep -Eo "db device.*$" | sed 's/db device.*\/dev\///';`
   fi
 fi
-
 
 
 echo "ceph-volume lvm zap $DEV"
