4bbe3ed8ad4110c48221661c135c58600bc6ee6e
Dan van der Ster <daniel.vanderster@cern.ch>
2019-08-23 13:28:40 +0200

    rbd-prefix-to-name: better with rados bindings

--- tools/rbd-prefix-to-name.py
+++ tools/rbd-prefix-to-name.py
@@ -1,27 +1,26 @@
 #!/usr/bin/python
 
 from optparse import OptionParser
+import rados
 import sys
-import re
-import commands
 
 def rbd_prefix_to_name(options):
-  prefix = options.prefix.replace('rbd_data.','id_')
+  assert(options.prefix.startswith('rbd_data.'))
+  key = options.prefix.replace('rbd_data.','id_')
   pool = options.pool
-  cmd = 'rados getomapval -p %s rbd_directory %s | grep \'^[0-9]\'' % (pool, prefix)
-  output = commands.getoutput(cmd)
-  name = ''
-  lines = [x.strip() for x in output.split("\n")]
-  for line in lines:
-    all = re.findall(r'[A-Za-z0-9\-]+', line)
-    if all:
-      name += all[-1]
-  return name
+  cluster = rados.Rados(conffile='/etc/ceph/ceph.conf')
+  cluster.connect()
+  ioctx = cluster.open_ioctx(pool)
+  with rados.ReadOpCtx(ioctx) as read_op:
+    iter, ret = ioctx.get_omap_vals_by_keys(read_op,(key,))
+    assert(ret == 0)
+    ioctx.operate_read_op(read_op, "rbd_directory")
+    try:
+      print list(iter)[0][1][4:]
+    except IndexError:
+      print 'Error: %s not found in pool %s' % (key, pool)
+      sys.exit(-1)
 
-#[09:51][root@cephmon0 (production:beesly/mon*1) ~]# rados getomapval -p volumes rbd_directory id_1a871195a024a98 -o volnam
-# (length 47) : 0000 : 2b 00 00 00 76 6f 6c 75 6d 65 2d 63 62 35 36 34 : +...volume-cb564
-#0010 : 34 33 36 2d 35 33 32 32 2d 34 61 32 36 2d 61 38 : 436-5322-4a26-a8
-#0020 : 61 31 2d 30 33 64 61 30 64 65 61 65 36 63 33    : a1-03da0deae6c3
 
 if __name__ == "__main__":
   parser = OptionParser()
@@ -33,4 +32,4 @@ if __name__ == "__main__":
   if not options.prefix:
     parser.error('PREFIX not given')
 
-  print rbd_prefix_to_name(options)
+  rbd_prefix_to_name(options)
